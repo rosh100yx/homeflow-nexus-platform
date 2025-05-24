6fb090955ffacc9c113dfbaaffef330646608eba
rosh100yx <rosh.100yx@gmail.com>
2025-05-24 09:15:04 +0530

    vite config changes for localhost

--- vite.config.ts
+++ vite.config.ts
@@ -4,14 +4,24 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/homeflow-nexus-platform/", // Add this line for GitHub Pages
+  // base: '/homeflow-nexus-platform/', // Add this line for GitHub Pages
   server: {
     host: "::",
     port: 8080,
   },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+    chunkSizeWarningLimit: 1600,
+  }
 }));
